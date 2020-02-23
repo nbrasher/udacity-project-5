58b2b186232ce18c2ee03ac36202fc36b62f5ade
Nathan Brasher <nathan.brasher@bain.com>
2020-02-22 21:26:56 -0600

    feature: add a-star

--- student_code.py
+++ student_code.py
@@ -1,36 +1,87 @@
-from math import sqrt, inf
+import networkx as nx
+import pickle
+import math
+import os
 
-def get_distance(x: list, y: list):
-    ''' Find Euclidean distance between two
-        points '''
+class Map:
+	def __init__(self, G):
+		self._graph = G
+		self.intersections = nx.get_node_attributes(G, "pos")
+		self.roads = [list(G[node]) for node in G.nodes()]
 
-    return sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+	def save(self, filename):
+		with open(filename, 'wb') as f:
+			pickle.dump(self._graph, f)
+
+
+def load_map(name):
+	with open(name, 'rb') as f:
+		G = pickle.load(f)
+	return Map(G)
+
+
+def dist(x: list, y: list):
+    # Find Euclidean distance between two points
+    return math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+
+
+def reconstruct_path(came_from: dict, current: int):
+    # Reconstruct shortest path derived by A*
+    path = [current]
+    while current in came_from.keys():
+        current = came_from[current]
+        path = [current] + path
+    
+    return path
 
 
 def shortest_path(M, start, goal):
-    # Implement Djikstra's algorithm for graph search
-    distance_dict = {node: inf for node in M.intersections.keys()}
-    shortest_path_to_node = dict()
+    ''' A* graph search
 
-    # Initialize tracking dictionaries
-    distance_dict[start] = 0
-    shortest_path_to_node[start] = [start]
-    
-    while distance_dict:
+        Parameters
+        ______
+        M (Map): Current graph
+        start (int): Node for start of search
+        goal (int): Node for search goal
+
+        Returns
+        ______
+        List(int): List of nodes in shortest path from start to goal
+    '''
+
+    # G- and F-score for path from start to each node
+    g = {node: math.inf for node in M.intersections.keys()}
+    f = {node: math.inf for node in M.intersections.keys()}
+    g[start] = 0
+    f[start] = dist(M.intersections[start], M.intersections[goal])
+
+    # Queue of nodes to be searched, sorted by distance estimate
+    search = {start: f[start]}
+
+    # Dict for tracking shortest path to each node from start node
+    came_from = {}
+
+    while search.keys():
         # Pop the shortest path 
-        current, distance = sorted(distance_dict.items(), key=lambda x: x[1])[0]
-        distance_dict.pop(current)
+        current, f_est = sorted(search.items(), key=lambda x: x[1])[0]
+        search.pop(current)
 
-        for edge in M.roads[current]:
-            if edge in distance_dict:
-                # Get distance and path to each edge
-                new_node_distance = distance + get_distance(M.intersections[current],
-                                                M.intersections[edge])
-                new_node_path = shortest_path_to_node[current] + [edge]
+        if current == goal:
+            return reconstruct_path(came_from, current)
+        
+        for neighbor in M.roads[current]:
+            # Get new distance to neighbor
+            g_est = g[current] + dist(M.intersections[current],
+                                      M.intersections[neighbor])
 
-                # If new path to the edge is shorter, update trackers
-                if distance_dict[edge] > new_node_distance:
-                    distance_dict[edge] = new_node_distance
-                    shortest_path_to_node[edge] = new_node_path
-    
-    return shortest_path_to_node[goal]
+            # If new distance is shorter, update paths
+            if g_est < g[neighbor]:
+                came_from[neighbor] = current
+                g[neighbor] = g_est
+                f[neighbor] = g_est + dist(M.intersections[neighbor],
+                                           M.intersections[goal])
+                if neighbor not in search.keys():
+                    search[neighbor] = f[neighbor]
+
+    # If no path found and no nodes left to search, return None for search failure
+    return
--- test_map.py
+++ test_map.py
@@ -1,6 +1,5 @@
+from student_code import load_map, shortest_path, dist, reconstruct_path
 from math import isclose
-from helpers import load_map
-from student_code import shortest_path, get_distance
 
 MAP_40_ANSWERS = [
     (5, 34, [5, 16, 37, 12, 34]),
@@ -13,8 +12,16 @@ def test_distance():
     x = [0.0, 0.0]
     y = [3.0, 4.0]
 
-    assert isclose(get_distance(x,x), 0.0)
-    assert isclose(get_distance(x,y), 5.0)
+    assert isclose(dist(x,x), 0.0)
+    assert isclose(dist(x,y), 5.0)
+
+def test_reconstruct():
+    # Test function to reconstruct path
+    came_from = {1: 2, 2: 3, 3: 5, 4: 3}
+
+    assert reconstruct_path(came_from, 1) == [5, 3, 2, 1]
+    assert reconstruct_path(came_from, 4) == [5, 3, 4]
+    assert reconstruct_path(came_from, 5) == [5]
 
 def test_simple_path():
     # Test single point
